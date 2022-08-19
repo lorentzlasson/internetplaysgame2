c22908fe57ba2e0e4749cd736ba7bcf47cffba40
Lorentz Lasson <lasson.lorentz@gmail.com>
2022-08-20 01:42:35 +0200

    Restructure entity state

--- backend/src/game.ts
+++ backend/src/game.ts
@@ -4,10 +4,28 @@ type State = {
   height: number
   width: number
   score: number
-  avatar: Position
-  coin: Position
-  bomb: Position
+  entities: Entity[]
+  // TODO: possible to have position as key in map?
+  // entities: { [key in Position]: Entity }
 }
+
+type BaseEntity = {
+  position: Position
+}
+
+type Avatar = BaseEntity & {
+  __type: 'avatar'
+}
+
+type Coin = BaseEntity & {
+  __type: 'coin'
+}
+
+type Bomb = BaseEntity & {
+  __type: 'bomb'
+}
+
+type Entity = Avatar | Coin | Bomb
 
 const DIRECTIONS = ['up', 'down', 'left', 'right'] as const
 
@@ -15,6 +33,13 @@ type Direction = typeof DIRECTIONS[number]
 
 export const isDirection = (token: any): token is Direction =>
   DIRECTIONS.includes(token)
+
+const isCoin = (entity: Entity): entity is Coin => entity.__type === 'coin'
+
+const isAvatar = (entity: Entity): entity is Avatar =>
+  entity.__type === 'avatar'
+
+const isBomb = (entity: Entity): entity is Bomb => entity.__type === 'bomb'
 
 const MOVES: {
   [key in Direction]: Move
@@ -29,19 +54,34 @@ const state: State = {
   height: 3,
   width: 3,
   score: 0,
-  avatar: [0, 2],
-  coin: [2, 0],
-  bomb: [0, 1],
+  entities: [
+    {
+      __type: 'avatar',
+      position: [0, 2],
+    },
+    {
+      __type: 'coin',
+      position: [2, 0],
+    },
+    {
+      __type: 'bomb',
+      position: [0, 1],
+    },
+  ],
 }
 
 const positionIsAllowed = ([x, y]: Position): boolean =>
   x >= 0 && x < state.width && y >= 0 && y < state.height
 
 const positionIsCoin = ([x, y]: Position): boolean =>
-  x === state.coin[0] && y === state.coin[1]
+  state.entities.some(
+    (e) => isCoin(e) && e.position[0] === x && e.position[1] === y
+  )
 
 const positionIsBomb = ([x, y]: Position): boolean =>
-  x === state.bomb[0] && y === state.bomb[1]
+  state.entities.some(
+    (e) => isBomb(e) && e.position[0] === x && e.position[1] === y
+  )
 
 const randomCapped = (cap: number) => Math.floor(Math.random() * cap)
 const randomPosition = (): Position => [
@@ -50,11 +90,15 @@ const randomPosition = (): Position => [
 ]
 
 const respawnCoin = () => {
-  state.coin = randomPosition()
+  const coin = state.entities.find(isCoin)
+  if (!coin) throw new Error('bomb not found')
+  coin.position = randomPosition()
 }
 
 const respawnBomb = () => {
-  state.bomb = randomPosition()
+  const bomb = state.entities.find(isBomb)
+  if (!bomb) throw new Error('bomb not found')
+  bomb.position = randomPosition()
 }
 
 const collectCoin = () => {
@@ -68,7 +112,10 @@ const blowUpBomb = () => {
 }
 
 export const move = (direction: Direction): State => {
-  const [x, y] = state.avatar
+  const avatar = state.entities.find(isAvatar)
+  if (!avatar) throw new Error('avatar not found')
+  const [x, y] = avatar.position
+
   const [mX, mY] = MOVES[direction]
   const newPos: Position = [x + mX, y + mY]
 
@@ -76,7 +123,7 @@ export const move = (direction: Direction): State => {
     return state
   }
 
-  state.avatar = newPos
+  avatar.position = newPos
 
   if (positionIsCoin(newPos)) {
     collectCoin()
