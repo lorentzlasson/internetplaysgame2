1be7bab49fce5a21f607b31e37ac2ac8e28df840
Lorentz Lasson <lasson.lorentz@gmail.com>
2022-10-23 23:48:52 +0200

    Init deno ssr

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
 import { serve } from 'https://deno.land/std@0.160.0/http/server.ts'
+import { getState } from './game.ts'
 import ui from './ui/main.tsx'
 
 const PORT = 8000
 
+const x = getState()
+
 await serve(
   (req) => {
-    const { searchParams: query } = new URL(req.url)
-    const name = query.get('name') || 'world'
-    const res = ui(name)
-    return new Response(res, {
-      headers: {
-        'content-type': 'text/html',
-      },
-    })
+    if (req.method === 'GET') {
+      const res = ui(x)
+      return new Response(res, {
+        headers: {
+          'content-type': 'text/html',
+        },
+      })
+    }
+
+    if (req.method === 'POST') {
+      return new Response(null, { status: 405 })
+    }
+
+    return new Response(null, { status: 405 })
   },
   { port: PORT }
 )
--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,13 +1,42 @@
 import { React, ReactDOMServer } from "../dep.ts";
+import { HEIGHT, isSamePosition, range, State, WIDTH } from "../common.ts";
 
-const ui = (x: string) => (
+const ICONS = {
+  bomb: "💣",
+  coin: "🪙",
+  avatar: "🏃",
+  blank: "⬜",
+  timerBar: "🟩",
+};
+
+const ui = (state: State) => (
   <html>
-    <body>
+    <head>
+      <title>Internet Plays Game</title>
+      <meta charSet="UTF-8"></meta>
+    </head>
+    <body style={{ display: "flex", overflow: "hidden" }}>
       <div>
-        <h1>Hello {x}!</h1>
+        <table style={{ fontSize: "100px" }}>
+          {range(HEIGHT).map((y) => (
+            <tr>
+              {range(WIDTH).map((x) => {
+                const entity = state.entities.find((e) =>
+                  isSamePosition(e.position, [x, y])
+                );
+                const emoji = entity ? ICONS[entity.__type] : ICONS.blank;
+                return (
+                  <td>
+                    {emoji}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </table>
       </div>
     </body>
   </html>
 );
 
-export default (data: string) => ReactDOMServer.renderToString(ui(data));
+export default (state: State) => ReactDOMServer.renderToString(ui(state));
